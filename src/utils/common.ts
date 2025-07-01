6f9492b5576741cf2b87a23f630accc0d89d5455
Conor Thompson <conorthompson213@gmail.com>
2025-07-01 17:48:15 +0100

    fixed wrong cdn link

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,27 +1,23 @@
 export function getHostEndpoint() {
-    const { protocol, hostname } = window.location; // Use `hostname` instead of `host`
-    console.log("Protocol:", protocol);
-    console.log("Hostname:", hostname);
-    return `${protocol}//${hostname}`;
-    //return 'http://192.168.0.23'
+  const { protocol, hostname } = window.location; // Use `hostname` instead of `host`
+  return `${protocol}//${hostname}`;
 }
 
 export function getHostAPIEndpoint() {
-    return `${getHostEndpoint()}:8081/api`;
+  return `${getHostEndpoint()}:8081/api`;
 }
 
 export function getCdnHostEndpoint() {
-    return 'http://192.168.0.23:8000';
-    //return `${getHostEndpoint()}:8000`
+  return `${getHostEndpoint()}:8000`
 }
 
-export   const scrollContainer = (containerId: string, direction: "left" | "right") => {
-    const container = document.getElementById(containerId);
-    if (container) {
-      const scrollAmount = 300;
-      container.scrollBy({
-        left: direction === "left" ? -scrollAmount : scrollAmount,
-        behavior: "smooth",
-      });
-    }
-  };
+export const scrollContainer = (containerId: string, direction: "left" | "right") => {
+  const container = document.getElementById(containerId);
+  if (container) {
+    const scrollAmount = 300;
+    container.scrollBy({
+      left: direction === "left" ? -scrollAmount : scrollAmount,
+      behavior: "smooth",
+    });
+  }
+};
